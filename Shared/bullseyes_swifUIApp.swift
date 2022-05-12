85a0b6bcee8e5afe2defe2247c3b52a3e731f590
Jackson Cuevas <jackson.cuevas@hotmail.com>
2022-05-12 12:17:14 -0400

    Initial Commit

---
+++ Shared/bullseyes_swifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  bullseyes_swifUIApp.swift
+//  Shared
+//
+//  Created by Jackson Cuevas on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct bullseyes_swifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
